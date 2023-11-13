f38b4400ce3c3aeba442b79b44e0c1570545a9f8
kkirrin <khlebnikov-2016@mail.ru>
2023-11-13 16:16:22 +1000

    feat: очень большой комит, добавил адаптив для анимации, оставил саму анимацию на мобилке, ссылки

--- js/modules/pin-wrap.js
+++ js/modules/pin-wrap.js
@@ -1,10 +1,19 @@
-export const initPinWrap = () => {
+export const handleScroll = () => {
     const $pinWrap = document.querySelector('.pin-wrap');
+    const maxScrollValue = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = window.scrollY / maxScrollValue;
+    const translateXValue = -100 * scrollPercentage;
 
-    window.addEventListener('scroll', () => {
-        const maxScrollValue = document.documentElement.scrollHeight - window.innerHeight;
-        const scrollPercentage = window.scrollY / maxScrollValue;
-        const translateXValue = -100 * scrollPercentage;
-        $pinWrap.style.transform = `translateX(${translateXValue}%)`;
-    });
-}
+    // Округляем значения до двух знаков после запятой
+    const roundedTranslateXValue = Math.round(translateXValue * 100) / 100;
+
+    $pinWrap.style.transform = `translateX(${roundedTranslateXValue}%)`;
+};
+
+export const initPinWrap = () => {
+    window.addEventListener('scroll', handleScroll);
+
+    if (window.innerWidth < 767) {
+        window.removeEventListener('scroll', handleScroll);
+    }
+};
--- js/observer.js
+++ js/observer.js
@@ -38,7 +38,7 @@ export const initObserver = () => {
         const options = {
             root: null, // используем вьюпорт в качестве корневого элемента
             rootMargin: '0px', // без отступов
-            threshold: 1 // коллбек будет вызван, когда элемент полностью виден
+            threshold: 0.1 // коллбек будет вызван, когда элемент полностью виден
         };
 
         const observer_2 = new IntersectionObserver((entries, observer) => {
@@ -54,6 +54,37 @@ export const initObserver = () => {
         }, options);
 
         observer_2.observe(svgAnimation_2);
+    }
+
+
+
+
+    const svgAnimation_3 = document.querySelector('.svg-animate-3');
+
+    if (svgAnimation_3) {
+        // Ищу внутри переменной
+        const svgs_3 = svgAnimation_3.querySelectorAll('svg');
+        // console.log(svgs_2)s
+
+        const options = {
+            root: null, // используем вьюпорт в качестве корневого элемента
+            rootMargin: '0px', // без отступов
+            threshold: 0.1 // коллбек будет вызван, когда элемент полностью виден
+        };
+
+        const observer_3 = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // Добавляем класс 'active' всем кнопкам
+                    svgs_3.forEach(svg => svg.classList.add('animate'));
+                    console.log('Element is in the viewport 333333333333333: ', entry.target);
+                    // Прекращаем наблюдение за элементом, так как класс уже добавлен
+                    observer_3.unobserve(entry.target);
+                }
+            });
+        }, options);
+
+        observer_3.observe(svgAnimation_3);
     }
 };
 
